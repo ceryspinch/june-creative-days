b905f35b02e4181af20f8afbcfddb3b22b61da91
Cerys Pinch <cerys.pinch@hpe.com>
2023-06-27 13:49:29 +0100

    Implement server methods

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module ticket_service
 go 1.20
 
 require (
+	github.com/google/uuid v1.3.0
 	google.golang.org/grpc v1.56.1
 	google.golang.org/protobuf v1.31.0
 	gorm.io/driver/postgres v1.5.2
--- server/main.go
+++ server/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"flag"
 	"fmt"
 	"log"
@@ -8,6 +10,7 @@ import (
 
 	pb "ticket_service/proto"
 
+	"github.com/google/uuid"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -55,4 +58,105 @@ var (
 
 type server struct {
 	pb.UnimplementedTicketManagerServer
+}
+
+// Create
+func (*server) BuyTicket(ctx context.Context, req *pb.BuyTicketRequest) (*pb.BuyTicketResponse, error) {
+	fmt.Println("Buy Ticket")
+
+	// Create a new ID for the ticket
+	newTicketID := uuid.New().String()
+
+	// Create the ticket data
+	data := Ticket{
+		ID:                newTicketID,
+		Purchaser:         req.Purchaser,
+		IsBringingGuest:   req.IsBringingGuest,
+		HasReceivedTicket: false,
+	}
+
+	// Add new ticket to the database
+	res := DB.Create(&data)
+	if res.RowsAffected == 0 {
+		return nil, errors.New("ticket creation unsuccessful")
+	}
+
+	return &pb.BuyTicketResponse{
+		Ticket: &pb.Ticket{
+			Id:                newTicketID,
+			Purchaser:         req.Purchaser,
+			IsBringingGuest:   req.IsBringingGuest,
+			HasReceivedTicket: false,
+		},
+	}, nil
+}
+
+// Get
+func (*server) ListTicket(ctx context.Context, req *pb.ListTicketRequest) (*pb.ListTicketResponse, error) {
+	fmt.Println("List Ticket", req.GetId())
+
+	var ticket Ticket
+
+	// Find the ticket with the same ID as the request in the database
+	res := DB.Find(&ticket, "id = ?", req.GetId())
+	if res.RowsAffected == 0 {
+		return nil, errors.New("ticket not found")
+	}
+
+	return &pb.ListTicketResponse{
+		Ticket: &pb.Ticket{
+			Id:                ticket.ID,
+			Purchaser:         ticket.Purchaser,
+			IsBringingGuest:   ticket.IsBringingGuest,
+			HasReceivedTicket: ticket.HasReceivedTicket,
+		},
+	}, nil
+}
+
+// Get
+func (*server) ListAllTickets(ctx context.Context, req *pb.ListAllTicketsRequest) (*pb.ListAllTicketsResponse, error) {
+	fmt.Println("List All Tickets")
+
+	tickets := []*pb.Ticket{}
+
+	// Find all tickets in the database
+	res := DB.Find(&tickets)
+	if res.RowsAffected == 0 {
+		return nil, errors.New("ticket not found")
+	}
+
+	return &pb.ListAllTicketsResponse{
+		Tickets: tickets,
+	}, nil
+}
+
+func (*server) UpdateTicketInformation(ctx context.Context, req *pb.UpdateTicketInformationRequest) (*pb.UpdateTicketInformationResponse, error) {
+	fmt.Println("Update Ticket")
+	var ticket Ticket
+
+	res := DB.Model(&ticket).Where("id=?", req.Id).Updates(Ticket{ID: req.Id, Purchaser: ticket.Purchaser, IsBringingGuest: req.IsBringingGuest, HasReceivedTicket: req.HasReceivedTicket})
+	if res.RowsAffected == 0 {
+		return nil, errors.New("ticket not found")
+	}
+
+	return &pb.UpdateTicketInformationResponse{
+		Ticket: &pb.Ticket{
+			Id:                ticket.ID,
+			Purchaser:         ticket.Purchaser,
+			IsBringingGuest:   ticket.IsBringingGuest,
+			HasReceivedTicket: ticket.HasReceivedTicket,
+		},
+	}, nil
+}
+
+func (*server) DeleteTicket(ctx context.Context, req *pb.DeleteTicketRequest) (*pb.DeleteTicketResponse, error) {
+	fmt.Println("Delete Ticket")
+
+	var ticket Ticket
+	res := DB.Where("id = ?", req.GetId()).Delete(&ticket)
+	if res.RowsAffected == 0 {
+		return nil, errors.New("ticket not found")
+	}
+
+	return &pb.DeleteTicketResponse{}, nil
 }
